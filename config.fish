b7b70571860a82dd5b84f9f61243ec84a4fa4aaa
DIP <saumyadip.private@protonmail.com>
2025-12-23 18:14:44 +0530

    Update config.fish

--- config.fish
+++ config.fish
@@ -7,15 +7,11 @@ set fish_greeting
 alias t="tmux -u"
 alias tmux="tmux -u"
 alias ta="tmux -u a"
-alias n="nvim"
-alias y="yazi"
-alias ls="lsd"
-function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
-end
-alias cai="/usr/local/cai/cai_venv/bin/python /usr/local/cai/cai.py"
+alias n="bob run 0.11.5"
+alias l="lsd"
+alias z="zoxide"
+source "$HOME/.cargo/env.fish"
+
+
+# this is for zoxide
+zoxide init fish | source
