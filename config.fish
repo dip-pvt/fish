e0f1f0ad4220ed02a9ea615135be915b36cf387d
Saumyadip <saumyadip.social@gmail.com>
2026-09-05 20:57:06 +0530

    sep 5

--- config.fish
+++ config.fish
@@ -1,19 +1,30 @@
 if status is-interactive
-
 end
+
+function fish_user_key_bindings
+    fish_vi_key_bindings
+end
+
+set -gx EDITOR n
+set -gx VISUAL n
+
 
 set fish_greeting
 
 alias t="tmux -u"
 alias tmux="tmux -u"
 alias ta="tmux -u a"
-alias n="bob run 0.11.5"
+alias n="bob run 0.12.2"
 alias l="lsd"
 alias z="zoxide"
 alias bat="batcat"
 alias gs="~/.local/bin/git-scope"
 source "$HOME/.cargo/env.fish"
 
+# for tgpt
+function bro
+    tgpt -i "$argv"
+end
 
 # this is for zoxide
 zoxide init fish | source
